c8fadaf36a22abb865ac867a8eaa0ef6ea636f6a
Blisss712 <derekjoshreyes132@gmail.com>
2025-08-11 04:22:42 +0800

    generate HTML for the order summary

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -68,6 +68,7 @@ function updateCartQuantity(){
         console.log(cart)
 }
 
+
 document.querySelector('.js-products-grid').innerHTML = productsHTML
 document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
     button.addEventListener('click', () => {
